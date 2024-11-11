99ee9608169a5caf3d161bc7056eb6663f9799fb
Fuadseid <fuaddbus@gmail.com>
2024-11-11 23:33:35 +0300

    adding flag

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-
+import { Flag } from "./components/Flag";
 function App() {
   return (
-    <>
-
-    </>
+    <div className="font-bold text-2xl align-middle">
+   <Flag/>
+  
+    </div>
   );
 }
 
