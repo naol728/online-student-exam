09c554002ec341bfdcb137f1bdc78d506181eb41
naol <naolmeseret22@gmail.com>
2024-11-11 20:51:44 +0300

    button componet done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+
 function App() {
-  return <h2 className="bg-slate-400">lets get started</h2>;
+  return (
+    <>
+
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+
+export default function Buttons({children,type}) {
+  return (
+    <Button variant={type}>
+      {children}
+    </Button>
+  )
+}
