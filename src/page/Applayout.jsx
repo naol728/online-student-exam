e9e4df95ed3c91ed2cdab04deeef789b1a44253b
naol <naolmeseret22@gmail.com>
2024-11-12 13:25:13 +0300

    she knows

--- src/page/Applayout.jsx
+++ src/page/Applayout.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router-dom'
 export default function Applayout() {
   return (
-    <div>
+    <div className='h-screen'>
+      <div>
+         <Navbar />
+      </div>
+
+      <main>
+        <Outlet />
+      </main>
+      
+
       
     </div>
   )
