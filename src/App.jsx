853bf5fbe1a3fed86ba1f1c7ccb56a6c4d6a7d4e
Abdunesr <nesredinabdelah33@gmail.com>
2024-11-12 10:14:32 +0300

    adding nava bar ui to out repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import InputField from "./components/Inputfild";
+import Navbar from "./components/Navbar";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Buttons from "./Button";
+import Logo from "./Logo";
 
 export default function Navbar() {
   return (
     <div>
-      
+      <nav className="flex justify-between m-0 p-4 bg-slate-300 ">
+        <Logo />
+        <Buttons children={"Login"} type={"outlined"} />
+      </nav>
     </div>
-  )
+  );
 }
