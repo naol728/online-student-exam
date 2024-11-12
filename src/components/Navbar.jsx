8759836d20f3dcf9a557efab2652ce62aca4346b
naol <naolmeseret22@gmail.com>
2024-11-12 19:52:32 +0300

    done with login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Buttons from './Button';
 export default function Navbar() {
   return (
     <div>
-      <nav className="flex justify-between m-0 p-4 bg-slate-300 ">
+      <nav className="flex justify-between m-0 p-4 bg-blue-100 shadow-xl ">
         <Logo />
         <Buttons children={"Login"} type={"outlined"} />
       </nav>
