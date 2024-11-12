a484ebcec08ea07032eb27454cffaa300667c459
naol <naolmeseret22@gmail.com>
2024-11-12 22:29:26 +0300

    done with page not found

--- src/App.jsx
+++ src/App.jsx
@@ -5,30 +5,42 @@ import Error from "./page/Error";
 import Exam from "./page/Exam";
 import Home from "./page/Home";
 import Done from "./page/Done";
+import Pagenotfound from "./page/Pagenotfound";
 
 const router = createBrowserRouter([
   {
     element: <Applayout />,
+    errorElement:<Error />,
     children: [
       {
         path: "/",
         element: <Login />,
+        errorElement:<Error />,
       },
       {
         path: "/home",
         element: <Home />,
+        errorElement:<Error />,
       },
       {
         path: "/exam",
         element: <Exam />,
+        errorElement:<Error />,
       },
       {
         path: "/Error",
         element: <Error />,
+        errorElement:<Error />,
       },
       {
         path: "/done",
         element: <Done />,
+        errorElement:<Error />,
+      },
+      {
+        path: "*",
+        element: <Pagenotfound />,
+        errorElement:<Error />,
       },
     ],
   },
