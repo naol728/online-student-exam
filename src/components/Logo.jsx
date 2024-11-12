cbf5812a6b8afbca3f28b02274bdbfa5778e62c7
Fuadseid <fuaddbus@gmail.com>
2024-11-12 13:30:36 +0300

    adding router package

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,8 @@
+
 export default function Logo() {
   return (
     <div>
-      <h1 className="font-agdasima">Online Exam </h1>
+      <h1 className="font-Lobster text-2xl">Online Exam </h1>
     </div>
   );
 }
