6e73d64ea749b9a5861937d63bc55334f69d208a
naol <naolmeseret22@gmail.com>
2024-12-04 20:58:59 +0300

    configure eslint

--- src/page/Loading.jsx
+++ src/page/Loading.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import "../index.css"
+
+import "../index.css";
 export default function Loading() {
   return (
-    <div className='absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex items-center justify-center'>   
-    <div className='loader'> 
+    <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex items-center justify-center">
+      <div className="loader"></div>
     </div>
-    </div>
-  )
+  );
 }
