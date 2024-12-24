77ec651341ad72bb5a4cd3ba8e3deee28617bda9
naol <naolmeseret22@gmail.com>
2024-12-24 18:20:31 +0300

    done with popup page and showing message while the button is clicked

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
 import Logo from "./Logo";
 import Buttons from "./Button";
 import { useStudentdata } from "../context/Studentprovider";
+import { useState } from "react";
+import Popup from "./Popup";
+
 export default function Navbar() {
+  const [isopenpopup, setIsopenpopup] = useState(false);
   const { state } = useStudentdata();
-  console.log(state.invalidcredital);
+  const handleclose = () => {
+    setIsopenpopup(false);
+  };
+  const handleopen = () => {
+    setIsopenpopup(true);
+  };
   return (
     <div>
       <nav className="flex justify-between m-0 p-4 bg-blue-100 shadow-xl ">
         <Logo />
-        <Buttons type={"text"}>
+        <Buttons type={"text"} onclick={handleopen}>
           {!state.invalidcredital ? "help" : "log in"}
         </Buttons>
+        {
+          <Popup isOpen={isopenpopup} onClose={handleclose}>
+            <div className="flex flex-col justify-between w-full">
+              {!state.invalidcredital
+                ? "please enter the username and password given by your school administrator "
+                : " are you sure to logout "}
+            </div>
+          </Popup>
+        }
       </nav>
     </div>
   );
--- src/components/containerlist.jsx
+++ src/components/containerlist.jsx
@@ -1,17 +1,39 @@
-import Buttons from './Button'
-function containerlist({itemname}) {
-    return (
-        <li className="flex justify-between border-b-2 p-2">
-            <div>
-              {itemname}  
-            </div>
-           
+import { useState } from "react";
+import Buttons from "./Button";
+import Popup from "./Popup";
+function containerlist({ item }) {
+  const [isopenpopup, setIsopenpopup] = useState(false);
+  const handleclick = () => {
+    setIsopenpopup(true);
+  };
+  const handleclose = () => {
+    setIsopenpopup(false);
+  };
+  return (
+    <>
+      <li className="flex justify-between border-b-2 p-2">
+        <div>{item.coursename}</div>
 
-           <Buttons>
-            view
-            </Buttons> 
-        </li>
-    )
+        <Buttons onclick={handleclick}>view</Buttons>
+      </li>
+      <Popup isOpen={isopenpopup} onClose={handleclose}>
+        <div className="flex flex-col justify-between w-full">
+          <div className="flex w-full justify-between px-3">
+            <h2 className="font-bold text-xl">Course Name</h2>
+            <p className="font-semibold text-lg">{item.coursename}</p>
+          </div>
+          <div className="flex w-full justify-between px-3">
+            <h2 className="font-bold text-xl">Course Code</h2>
+            <p className="font-semibold text-lg">{item.coursecode}</p>
+          </div>
+          <div className="flex w-full justify-between px-3">
+            <h2 className="font-bold text-xl">Course Score</h2>
+            <p className="font-semibold text-lg">{item.coursescore}</p>
+          </div>
+        </div>
+      </Popup>
+    </>
+  );
 }
 
-export default containerlist
+export default containerlist;
